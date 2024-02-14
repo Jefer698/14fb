c2d703af908ac921086146c20ba31b512bfc4123
JEFER SURA <jmunoz14@sura.cl>
2024-02-14 00:58:27 -0300

    AGREGANDO MUSICA

--- scrypt.js
+++ scrypt.js
@@ -1,14 +1,21 @@
 const yesBtn = document.querySelector('#yesBtn');
 const card = document.querySelector('#card');
 const closeBtn = document.querySelector('#closeBtn');
+const audio = new Audio('musica.mp3'); // Ruta al archivo de audio
 
 yesBtn.addEventListener('click', function () {
     card.classList.remove('hidden');
+    // Reproducir la música de fondo
+    audio.loop = true; // Reproducir en bucle
+    audio.play();
 });
 
 
 closeBtn.addEventListener('click', function () {
     card.classList.add('hidden');
+    // Detener la música cuando se cierra la tarjeta
+    audio.pause();
+    audio.currentTime = 0; // Reiniciar el tiempo de reproducción al inicio
 });
 
 const noBtn = document.querySelector('#noBtn');
